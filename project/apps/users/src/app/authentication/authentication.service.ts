c4df5eb5643c9627015596348a63f151b660421d
lgmarch <lmarch@yandex.ru>
2023-04-04 00:47:44 +0300

    Добавит метод регистрации пользователя в сервис AuthenticationService.

--- project/apps/users/src/app/authentication/authentication.service.ts
+++ project/apps/users/src/app/authentication/authentication.service.ts
@@ -1,8 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { BlogUserMemoryRepository } from '../blog-user/blog-user-memory.repository';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserRole } from '@project/shared/shared-types';
+import dayjs from 'dayjs';
+import { BlogUserEntity } from '../blog-user/blog-user.entity';
+import { AUTH_USER_EXISTS } from './authentication.constant';
 
 @Injectable()
 export class AuthenticationService {
-  constructor(private readonly blogUserRepository: BlogUserMemoryRepository) {
+  constructor(
+    private readonly blogUserRepository: BlogUserMemoryRepository
+  ) {}
+
+  public async register(dto: CreateUserDto) {
+    const {name, email, password, avatar=''} = dto;
+
+    const existUser = await this.blogUserRepository
+      .findByEmail(email);
+
+    if(existUser) {
+      throw new ConflictException(AUTH_USER_EXISTS);
+    }
+
+    const blogUser = {
+      email, name, avatar, passwordHash: '', role: UserRole.User,
+      registrationDate: dayjs().toDate()
+    }
+
+    const userEntity = await new BlogUserEntity(blogUser)
+      .setPassword(password);
+
+    return this.blogUserRepository
+      .create(userEntity);
   }
 }
