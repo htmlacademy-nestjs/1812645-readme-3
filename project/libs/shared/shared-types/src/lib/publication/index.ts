0f6b4686d2d67109e1de62c8ab212bb642636bb8
lgmarch <lmarch@yandex.ru>
2023-04-10 15:09:43 +0300

    Добавит интерфейс для Publication.

---
+++ project/libs/shared/shared-types/src/lib/publication/index.ts
@@ -0,0 +1,4 @@
+export * from './posts.type';
+export * from './post-interfaces';
+export { PublicationStatus } from './publication-status.enum';
+export { IPublication } from './publication.interface';
