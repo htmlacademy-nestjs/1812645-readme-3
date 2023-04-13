f077735d7f4581ce1c8458be2b3761cef1a50f68
lgmarch <lmarch@yandex.ru>
2023-04-13 15:47:07 +0300

    Зарегистрирует модуль конфигурации в приложении Users.

--- project/apps/users/src/app/app.module.ts
+++ project/apps/users/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { BlogUserModule } from './blog-user/blog-user.module';
+import { ConfigUserModule } from '@project/config/config-users';
 
 @Module({
-  imports: [AuthenticationModule, BlogUserModule],
+  imports: [AuthenticationModule, BlogUserModule, ConfigUserModule],
   controllers: [],
   providers: [],
 })
