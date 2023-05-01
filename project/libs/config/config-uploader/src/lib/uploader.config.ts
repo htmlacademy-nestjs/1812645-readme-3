27a83934ed76a80c74d8fb78346f1a0883b6174f
lgmarch <lmarch@yandex.ru>
2023-05-01 17:00:38 +0300

    Добавит MongoDB к Upload.

--- project/libs/config/config-uploader/src/lib/uploader.config.ts
+++ project/libs/config/config-uploader/src/lib/uploader.config.ts
@@ -2,11 +2,20 @@ import { registerAs } from '@nestjs/config';
 import * as Joi from 'joi';
 
 const DEFAULT_PORT = 3000;
+const DEFAULT_MONGO_PORT = 27017;
 
 export interface UploaderConfig {
   environment: string;
   uploadDirectory: string;
   port: number;
+  db: {
+    host: string;
+    port: number;
+    user: string;
+    name: string;
+    password: string;
+    authBase: string;
+  }
 }
 
 export default registerAs('application', (): UploaderConfig => {
@@ -14,12 +23,28 @@ export default registerAs('application', (): UploaderConfig => {
     environment: process.env.NODE_ENV,
     uploadDirectory: process.env.UPLOAD_DIRECTORY_PATH,
     port: parseInt(process.env.PORT || DEFAULT_PORT.toString(), 10),
+    db: {
+      host: process.env.MONGO_HOST,
+      port: parseInt(process.env.MONGO_PORT ?? DEFAULT_MONGO_PORT.toString(), 10),
+      name: process.env.MONGO_DB,
+      user: process.env.MONGO_USER,
+      password: process.env.MONGO_PASSWORD,
+      authBase: process.env.MONGO_AUTH_BASE,
+    }
   };
 
   const validationSchema = Joi.object<UploaderConfig>({
     environment: Joi.string().valid('development', 'production', 'stage'),
     uploadDirectory: Joi.string(),
-    port: Joi.number().port().default(DEFAULT_PORT)
+    port: Joi.number().port().default(DEFAULT_PORT),
+    db: Joi.object({
+      host: Joi.string().valid().hostname(),
+      port: Joi.number().port(),
+      name: Joi.string().required(),
+      user: Joi.string().required(),
+      password: Joi.string().required(),
+      authBase: Joi.string().required(),
+    })
   });
 
   const { error } = validationSchema.validate(config, { abortEarly: true });
