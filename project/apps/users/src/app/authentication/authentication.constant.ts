204c20a9d01756c8e7ffb856b90d03dfbc6f171e
lgmarch <lmarch@yandex.ru>
2023-05-01 22:55:18 +0300

    Добавит базовую валидацию для DTO сервиса Users.

--- project/apps/users/src/app/authentication/authentication.constant.ts
+++ project/apps/users/src/app/authentication/authentication.constant.ts
@@ -1,3 +1,6 @@
 export const AUTH_USER_EXISTS = 'User with this email exists';
 export const AUTH_USER_NOT_FOUND = 'User not found';
 export const AUTH_USER_PASSWORD_WRONG = 'User password is wrong';
+export const AUTH_USER_EMAIL_NOT_VALID = 'The email is not valid';
+export const AUTH_USER_NAME_NOT_VALID = 'The name must contain from 3 to 50 characters';
+export const AUTH_USER_PASSWORD_NOT_VALID = 'The password must contain from 6 to 12 characters';
--- project/apps/users/src/app/authentication/dto/login-user.dto.ts
+++ project/apps/users/src/app/authentication/dto/login-user.dto.ts
@@ -1,15 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { AUTH_USER_EMAIL_NOT_VALID, AUTH_USER_PASSWORD_NOT_VALID } from '../authentication.constant';
+import { IsEmail, Length } from 'class-validator';
 
 export class LoginUserDto {
   @ApiProperty({
     description: 'User unique email',
     example: 'user@mail.ru'
   })
+  @IsEmail({}, { message: AUTH_USER_EMAIL_NOT_VALID })
   public email: string;
 
   @ApiProperty({
     description: 'User password',
     example: '123'
   })
+  @Length(6, 12, { message: AUTH_USER_PASSWORD_NOT_VALID })
   public password: string;
 }
