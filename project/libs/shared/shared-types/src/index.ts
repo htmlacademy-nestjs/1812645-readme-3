a77cf94270482e5048996576c9ab473bc17d434f
lgmarch <lmarch@yandex.ru>
2023-05-01 18:07:34 +0300

    Добавит сохранение информации о загруженных файлах в MongoDB.

--- project/libs/shared/shared-types/src/index.ts
+++ project/libs/shared/shared-types/src/index.ts
@@ -4,3 +4,4 @@ export * from './lib/publication/index';
 export * from './lib/comment.interface';
 export * from './lib/like.interface';
 export * from './lib/tag.interface';
+export * from './lib/file.interface';
