07b3f2745704a85c3ddbb646e2bf29e7d7bb18d4
lgmarch <lmarch@yandex.ru>
2023-05-09 10:30:49 +0300

    Подключит HttpModule в app.module.ts.

--- project/apps/bff/src/app/app.module.ts
+++ project/apps/bff/src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
 import { PublicationsController } from './publications.controller';
 import { UsersController } from './users.controller';
+import { HttpModule } from '@nestjs/axios';
+import { HTTP_CLIENT_MAX_REDIRECTS, HTTP_CLIENT_TIMEOUT } from './app.config';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [
+  imports: [
+    HttpModule.register({
+      timeout: HTTP_CLIENT_TIMEOUT,
+      maxRedirects: HTTP_CLIENT_MAX_REDIRECTS,
+    }),
+  ],
+  controllers: [
     PublicationsController,
     UsersController,
   ],
+  providers: [],
 })
 export class AppModule {}
