0f74ffde79a41820256e330ec5229d6901f12a52
lgmarch <lmarch@yandex.ru>
2023-05-09 03:58:25 +0300

    Добавит интерфейс Token.

--- project/libs/shared/shared-types/src/index.ts
+++ project/libs/shared/shared-types/src/index.ts
@@ -9,3 +9,5 @@ export * from './lib/token-payload.interface';
 export * from './lib/subscriber.interface';
 export * from './lib/rabbit-routing.enum';
 export * from './lib/request-with.user';
+export * from './lib/token.interface';
+export * from './lib/refresh-token-payload.interface';
