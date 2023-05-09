1f5eed5c640ebe8dfeea3558fb716a57a67cb4a3
lgmarch <lmarch@yandex.ru>
2023-05-09 03:49:00 +0300

    Изменит номер порта, чтобы избежать конфликтов.

--- project/apps/publications/src/main.ts
+++ project/apps/publications/src/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
     transform: true,
   }));
 
-  const port = process.env.PORT || 3333;
+  const port = 3333;
   await app.listen(port);
   Logger.log(
     `🚀 Application * Publications * is running on: http://localhost:${port}/${globalPrefix}`
